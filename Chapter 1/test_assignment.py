36c7b30e12750c945e440e8be1a7e67cb6f7c568
mrw <lwilmoth>
2025-01-06 13:42:17 -0500

    added chapter 1 assignment

---
+++ Chapter 1/test_assignment.py
@@ -0,0 +1,34 @@
+import pytest
+
+from assignment import add_matrices, scalar_mult, multiply_matrices, multiply_matrices_1, multiply_matrices_2 , probability_distributions 
+from assignment import probability_distributions_2, statistical_tests
+def test_add_matrices():
+    assert add_matrices([[1, 2], [3, 4]], [[2, 2], [2, 2]]) == [[3, 4], [5, 6]]
+    assert add_matrices([[1, 0], [0, 1]], [[0, 1], [1, 0]]) == [[1, 1], [1, 1]]
+    assert add_matrices([[1, 2, 3]], [[4, 5, 6]]) == [[5, 7, 9]]
+
+def test_scalar_mult():
+    assert scalar_mult(3, [[1, 2], [3, 4]]) == [[3, 6], [9, 12]]
+    assert scalar_mult(0, [[1, 2], [3, 4]]) == [[0, 0], [0, 0]]
+    assert scalar_mult(-1, [[1, -2], [-3, 4]]) == [[-1, 2], [3, -4]]
+
+def test_multiply_matrices_1():
+    assert multiply_matrices_1() == [[32]]
+
+def test_multiply_matrices_2():
+    assert multiply_matrices_2() == [[22, 28], [49, 64]]
+
+def test_multiply_matrices():
+    assert multiply_matrices([[1, 2, 3]], [[4], [5], [6]]) == [[32]]
+    assert multiply_matrices([[1, 2, 3], [4, 5, 6]], [[1, 2], [3, 4], [5, 6]]) == [[22, 28], [49, 64]]
+    assert multiply_matrices([[1, 2]], [[1, 2], [3, 4]]) == None
+
+def test_probability_distributions():
+    assert probability_distributions().title() == "Uniform Distribution"
+
+def test_probability_distributions_2():
+    assert (probability_distributions_2().title() == "Normal Distribution") or (probability_distributions_2().title() == "Gaussian Distribution")
+
+
+def test_statistical_tests():
+    assert statistical_tests() == (ord("A") - 60) /100
